9e8579de90a5d857e8340d9e3ae1ddb03af4132b
spainisnotequal <spainisnotequal@gmail.com>
2020-03-26 18:53:55 +0100

    Map over an internal variable to create the table

--- client/src/components/AppTable.js
+++ client/src/components/AppTable.js
@@ -2,6 +2,40 @@ import React from "react";
 import { Table } from "reactstrap";
 
 const AppTable = props => {
+  const data = [
+    {
+      _id: "5e7b987b67432541c5111420",
+      date: "2020-01-14T23:00:00.000Z",
+      gold_price: 1556.09,
+      silver_price: 17.98,
+      __v: 0
+    },
+    {
+      _id: "5e7b981167432541c511141f",
+      date: "2020-01-13T23:00:00.000Z",
+      gold_price: 1546.17,
+      silver_price: 17.78,
+      __v: 0
+    },
+    {
+      _id: "5e7b97be67432541c511141e",
+      date: "2020-01-12T23:00:00.000Z",
+      gold_price: 1547.8,
+      silver_price: 17.95,
+      __v: 0
+    }
+  ];
+  const rows = data.map(datum => {
+    const { _id, date, gold_price, silver_price } = datum;
+    return (
+      <tr id={_id}>
+        <th scope="row">{date}</th>
+        <td>{gold_price}</td>
+        <td>{silver_price}</td>
+      </tr>
+    );
+  });
+
   return (
     <Table bordered hover>
       <thead>
@@ -11,23 +45,7 @@ const AppTable = props => {
           <th>Silver price ($)</th>
         </tr>
       </thead>
-      <tbody>
-        <tr>
-          <th scope="row">09/01/2020</th>
-          <td>1500</td>
-          <td>15</td>
-        </tr>
-        <tr>
-          <th scope="row">10/01/2020</th>
-          <td>1600</td>
-          <td>16</td>
-        </tr>
-        <tr>
-          <th scope="row">11/01/2020</th>
-          <td>1700</td>
-          <td>17</td>
-        </tr>
-      </tbody>
+      <tbody>{rows}</tbody>
     </Table>
   );
 };
