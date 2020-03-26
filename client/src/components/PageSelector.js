a9faa9d0e84f6dd829ce685ee3b447ab2d4b3a7e
spainisnotequal <spainisnotequal@gmail.com>
2020-03-26 10:43:49 +0100

    Add images to the PageSelector component

--- client/src/components/PageSelector.js
+++ client/src/components/PageSelector.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import goldImage from "../images/gold.jpg";
+import silverImage from "../images/silver.jpg";
+
 const PageSelector = () => {
   return (
     <div class="container-fluid" color="dark">
-      <div class="row text-center text-white">
-        <div class="col-sm text-center bg-dark m-2">Gold</div>
-        <div class="col-sm text-center bg-dark m-2">Silver</div>
+      <div class="row text-center">
+        <div class="col-sm bg-dark m-2">
+          <a href="*">
+            <img src={goldImage} class="img-fluid pt-3 pb-2" alt="Gold"></img>
+            <h2 class="text-white">Gold</h2>
+          </a>
+        </div>
+        <div class="col-sm bg-dark m-2">
+          <a href="*">
+            <img
+              src={silverImage}
+              class="img-fluid pt-3 pb-2"
+              alt="Silver"
+            ></img>
+            <h2 class="text-white">Silver</h2>
+          </a>
+        </div>
       </div>
     </div>
   );
