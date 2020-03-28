be73204bb0d34b20c136b4ae5340506c0cc7cbab
spainisnotequal <spainisnotequal@gmail.com>
2020-03-28 12:08:17 +0100

    Add configuration to be able to deploy to Heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 // in development, load environment variables (for instance, MONGODB_DEV)
 if (process.env.NODE_ENV !== "production") {
@@ -26,6 +27,16 @@ mongoose
 
 // use Routes
 app.use("/api/forexPrices", forexPrice);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // configure the port
 const PORT = process.env.PORT || 5000;
