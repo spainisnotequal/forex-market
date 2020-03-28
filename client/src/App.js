6c2197ba77debe563cba27d578868a0df4f06a4d
spainisnotequal <spainisnotequal@gmail.com>
2020-03-28 10:35:52 +0100

    Get data from the server

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 import AppNavbar from "./components/AppNavbar";
 import PageSelector from "./components/PageSelector";
 import AppTable from "./components/AppTable";
@@ -7,11 +9,14 @@ import AppFooter from "./components/AppFooter";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  axios.get("/api/forexPrices").then(res => setData(res.data));
+
   return (
     <div className="App">
       <AppNavbar />
       <PageSelector />
-      <AppTable />
+      <AppTable data={data} />
       <AppFooter />
     </div>
   );
--- client/src/components/AppTable.js
+++ client/src/components/AppTable.js
@@ -3,38 +3,11 @@ import { Table } from "reactstrap";
 import moment from "moment";
 
 const AppTable = props => {
-  const data = [
-    {
-      _id: "5e7b987b67432541c5111420",
-      date: "2020-01-14T23:00:00.000Z",
-      gold_price: 1556.09,
-      silver_price: 17.98,
-      __v: 0
-    },
-    {
-      _id: "5e7b981167432541c511141f",
-      date: "2020-01-13T23:00:00.000Z",
-      gold_price: 1546.17,
-      silver_price: 17.78,
-      __v: 0
-    },
-    {
-      _id: "5e7b97be67432541c511141e",
-      date: "2020-01-12T23:00:00.000Z",
-      gold_price: 1547.8,
-      silver_price: 17.95,
-      __v: 0
-    }
-  ];
-
-  const dataKeys = Object.keys(data[0]);
-  dataKeys.shift(); // remove the first item of an array (it's the "_id" field from MongoDB)
-  dataKeys.pop(); // remove the last item of an array (it's the "__v" field from MongoDB)
-
+  const dataKeys = ["Date (dd/mm/yyyy)", "Gold price ($)", "Silver price ($)"];
   const headerElements = dataKeys.map(element => <th>{element}</th>);
   const header = <tr>{headerElements}</tr>;
 
-  const rows = data.map(datum => {
+  const rows = props.data.map(datum => {
     const { _id, date, gold_price, silver_price } = datum;
 
     return (
